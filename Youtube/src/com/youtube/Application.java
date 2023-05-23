344cbbc5cf947b2d38be21f070fc6693498f0974
kwy8686 <133620229+kwy8686@users.noreply.github.com>
2023-05-23 15:17:48 +0900

    23.05.23
    Array 및 class

---
+++ Youtube/src/com/youtube/Application.java
@@ -0,0 +1,16 @@
+package com.youtube;
+
+import com.youtube.model.User;
+
+import com.youtube.model.Video;
+
+public class Application {
+
+	public static void main(String[] args) {
+		User user = new User();
+		
+		Video video = new Video();
+
+	}
+
+}
